08bc353b7f79ad3ab3b0376432fb127d91359c43
Francesco Bognini <info@francescobognini.com>
2023-03-02 14:50:38 +0100

    infrastructure - dictionary and listOfString converter

---
+++ src/fbognini.Infrastructure/Persistence/ValueConverters/DictionaryConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace fbognini.Infrastructure.Persistence.ValueConverters
+{
+    public class DictionaryConverter : ValueConverter<Dictionary<string, object>, string>
+    {
+        private static readonly JsonSerializerOptions options = new();
+
+        public DictionaryConverter()
+            : base(
+                v => JsonSerializer.Serialize(v, options),
+                v => JsonSerializer.Deserialize<Dictionary<string, object>>(v, options))
+        {
+        }
+    }
+}
---
+++ src/fbognini.Infrastructure/Persistence/ValueConverters/ListOfStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace fbognini.Infrastructure.Persistence.ValueConverters
+{
+    public class ListOfStringConverter : ValueConverter<List<string>, string>
+    {
+        private static readonly JsonSerializerOptions options = new();
+
+        public ListOfStringConverter()
+            : base(
+                v => JsonSerializer.Serialize(v, options),
+                v => JsonSerializer.Deserialize<List<string>>(v, options))
+        {
+        }
+    }
+}
