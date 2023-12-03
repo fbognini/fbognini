dc78084c5fc53828afe5d131fd49caaa1d10ac96
Francesco Bognini <info@francescobognini.com>
2023-12-03 04:17:19 +0100

    core - silent exception

---
+++ src/fbognini.Core/Exceptions/ISilentException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fbognini.Core.Exceptions
+{
+    public sealed class SilentException: Exception, ISilentException
+    {
+        public SilentException() { }
+        public SilentException(string message) : base(message) { }
+        public SilentException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    /// <summary>
+    /// If an exception implement ISilentException it won't be logged as an error by middleware
+    /// </summary>
+    public interface ISilentException
+    {
+    }
+}
+
