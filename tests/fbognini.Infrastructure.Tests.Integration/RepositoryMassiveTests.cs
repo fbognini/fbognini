498677ead2761133f9eebac229b130524d42cf25
Francesco Bognini <info@francescobognini.com>
2023-12-13 15:09:40 +0100

    infrastructure - tests for massive insert

---
+++ tests/fbognini.Infrastructure.Tests.Integration/RepositoryMassiveTests.cs
@@ -0,0 +1,70 @@
+﻿using Bogus;
+using EFCore.BulkExtensions;
+using fbognini.Core.Data;
+using fbognini.Infrastructure.Repositorys;
+using fbognini.Infrastructure.Tests.Integration.Fixture;
+using fbognini.Infrastructure.Tests.Integration.Fixture.Entities;
+using fbognini.Infrastructure.Tests.Integration.Fixture.Entities.Seeds;
+using fbognini.Infrastructure.Tests.Integration.Fixture.SearchCriterias;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fbognini.Infrastructure.Tests.Integration;
+
+public class RepositoryMassiveTests : IClassFixture<FullDatabaseFixture>
+{
+
+    private readonly IRepositoryAsync repository;
+    private readonly IntegrationTestsDbContext context;
+
+    public RepositoryMassiveTests(FullDatabaseFixture databaseFixture)
+    {
+        repository = databaseFixture.Repository;
+        context = databaseFixture.DbContext;
+    }
+
+    [Fact]
+    public async Task MassiveInsertAsync_InsertFullList_WhenOk()
+    {
+        var authors = AuthorSeed.Faker.Generate(50);
+
+        var cancellationToken = new CancellationToken();
+
+        var bulkConfig = new BulkConfig()
+        {
+            SetOutputIdentity = true,
+        };
+        await repository.MassiveInsertAsync(authors, bulkConfig, cancellationToken: cancellationToken);
+
+        var ids = authors.Select(x => x.Id).ToList();
+
+        var savedAuthors = context.Authors.Where(x => ids.Contains(x.Id)).ToList();
+
+        savedAuthors.Count.Should().Be(authors.Count);
+    }
+
+    [Fact]
+    public async Task MassiveInsertAsync_PopulateAuditableColumns_WhenAuditableEntityInserted()
+    {
+        var authors = AuthorSeed.Faker.Generate(50);
+
+        var cancellationToken = new CancellationToken();
+
+        var bulkConfig = new BulkConfig()
+        {
+            SetOutputIdentity = true,
+        };
+        await repository.MassiveInsertAsync(authors, bulkConfig, cancellationToken: cancellationToken);
+
+        authors.ForEach(author =>
+        {
+            author.Id.Should().BeGreaterThan(0);
+            author.Created.Should().NotBe(DateTime.MinValue);
+            author.LastUpdated.Should().NotBe(DateTime.MinValue);
+        });
+    }
+}
