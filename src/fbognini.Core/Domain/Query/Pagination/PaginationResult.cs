6e8297cf08e8c8fe5424ea41bc08c7f14cba331e
Francesco Bognini <info@francescobognini.com>
2024-09-29 05:54:37 +0200

    MaxTake for pagination count

--- src/fbognini.Core/Domain/Query/Pagination/PaginationResult.cs
+++ src/fbognini.Core/Domain/Query/Pagination/PaginationResult.cs
@@ -6,6 +6,7 @@ public class PaginationResult
     public int? PageNumber { get; set; }
     public int? PageSize { get; set; }
     public int? Total { get; set; }
+    public bool? AtLeast { get; set; }
     public string? ContinuationSince { get; set; }
     public int? PartialTotal { get; set; }
 }
